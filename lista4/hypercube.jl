d1124e3579ef6a66347cb710b240d8b4337af948
Norbert Jaśniewicz <norbert.jasniewicz@protonmail.com>
2023-06-11 18:41:25 +0200

    Added unit tests for hypercube

--- lista4/hypercube.jl
+++ lista4/hypercube.jl
@@ -1,8 +1,13 @@
 module HypercubeGraph
 
+import DataStructures
+
+export Hypercube, setEdgeWeight!, getEdgeWeight, getNeighbours, getReverseNeighbours, getAdjacentVertices,
+  bestFirstSearch, EdmondsKarp, hammingDistance, hammingWeight, randomHyperCube
+
 mutable struct Hypercube
   size::UInt8
-  # Index of edge v -> w is requal to v's value prepended by the pos of bit
+  # Index of edge v -> w is requal to v's value appended by the pos of bit
   # that remains set after v xor w
   capacity::Vector{UInt16}
 
@@ -23,14 +28,14 @@ end #Hypercube
   while xored > 0
     counter += xored & 1
     xored = xored >> 1
-  end
+  end #while 
 
   return counter
-end
+end #hammingDistance
 
 @inline hammingWeight(number::UInt16) = hammingDistance(number, UInt16(0))
 
-@inline function computeIndex(from::UInt16, to::UInt16)::UInt32
+@inline function computeIndex(cube::Hypercube, from::UInt16, to::UInt16)::UInt32
 
   index = 1
   counter::UInt32 = UInt32(0)
@@ -40,11 +45,9 @@ end
     index = index << 1
   end #while
 
-  counter = counter << 16
+  counter += from << UInt(ceil(log2(index)))
 
-  counter += from
-
-  return counter
+  return counter + 1
 end #computeIndex
 
 function setEdgeWeight!(cube::Hypercube, from::UInt16, to::UInt16, weight::UInt32)
@@ -52,20 +55,149 @@ function setEdgeWeight!(cube::Hypercube, from::UInt16, to::UInt16, weight::UInt3
     throw(error("Incorrect weight"))
   end #if
 
+  @boundscheck if from > 2^cube.size - 1 || to > 2^cube.size - 1
+    throw(error("Edge does not exist"))
+  end
+
   @boundscheck if hammingDistance(from, to) != 1
     throw(error("Edge does not exist"))
   end #if
 
-  index = computeIndex(from, to)
+  index = computeIndex(cube, from, to)
 
   #We are mapping vals from 1..2^16 to 0..2^16 - 1
   cube.capacity[index] = UInt16(weight - 1)
-end
+end #setEdgeWeight!
 
 function getEdgeWeight(cube::Hypercube, from::UInt16, to::UInt16)::UInt32
-  return UInt32(cube.capacity[computeIndex(from, to)]) + UInt32(1)
+  @boundscheck if from > 2^cube.size - 1 || to > 2^cube.size - 1
+    throw(error("Edge does not exist"))
+  end #if
+
+  @boundscheck if hammingDistance(from, to) != 1
+    throw(error("Edge does not exist"))
+  end #if
+
+  if hammingWeight(from) > hammingWeight(to)
+    return UInt32(0)
+  end #if
+
+  return UInt32(cube.capacity[computeIndex(cube, from, to)]) + UInt32(1)
+end #getEdgeWeight
+
+@inline function getNeighbours(cube::Hypercube, vertex::UInt16)::Vector{UInt16}
+  result::Vector{UInt16} = []
+  index::UInt16 = 2^(cube.size - 1)
+
+  while index > 0
+    if vertex & index == 0
+      push!(result, vertex | index)
+    end #if
+    index >>= 1
+  end #while
+
+  return result
+end #getNeighbours
+
+@inline function getReverseNeighbours(cube::Hypercube, vertex::UInt16)::Vector{UInt16}
+  result::Vector{UInt16} = []
+  index::UInt16 = 2^(cube.size - 1)
+
+  while index > 0
+    if vertex & index != 0
+      push!(result, vertex & ~index)
+    end #if
+    index >>= 1
+  end #while
+
+  return result
 end
 
+@inline function getAdjacentVertices(cube::Hypercube, vertex::UInt16)::Vector{UInt16}
+  result::Vector{UInt16} = []
+  index::UInt16 = typemax(UInt16) - (typemax(UInt16) >> 1)
 
+  while index > 0
+    push!(result, vertex ⊻ index)
+    index >>= 1
+  end #while
+
+  return result
+end #getAdjacentVertices
+
+@inline function bestFirstSearch(cube::Hypercube, flow::Dict{Tuple{UInt16,UInt16},Int64}, from::UInt16, to::UInt16)::Dict{UInt16,UInt16}
+  queue = DataStructures.Queue{UInt16}()
+
+  while !isempty(queue)
+    current = dequeue!(queue)
+    if current == to
+      break
+    end #if
+
+    parent = Dict{UInt16,UInt16}()
+
+    for neighbour in getNeighbours(cube, current)
+      if !hasKey(parent, neighbour) && neighbour != from && getEdgeWeight(cube, current, neighbour) > get(flow, (current, neighbour), 0)
+        enqueue!(queue, neighbour)
+        parent[neighbour] = current
+      end #if
+    end #for
+
+    for neighbour in getReverseNeighbours(cube, current)
+      if !hasKey(parent, current) && current != from && 0 > get(flow, (neighbour, current), 0)
+        enqueue!(queue, current)
+        parent[current] = neighbour
+      end #if
+    end #for
+  end #while
+
+  return parent
+end #bestFirstSearch
+
+function EdmondsKarp(cube::Hypercube, from::UInt16, to::UInt16)::Dict{Tuple{UInt16,UInt16},Int64}
+  flow::Dict{Tuple{UInt16,UInt16},Int64} = Dict()
+
+  while true
+    path = bestFirstSearch(cube, flow, from, to)
+
+    if !hasKey(path, to)
+      break
+    end #if
+
+    deltaFlow = typemax(UInt32)
+    current = to
+    #todo: change UInt32 to Int64
+    while current != from
+      deltaFlow = min(deltaFlow, getEdgeWeight(cube, path[current], current) - get(flow, (path[current], current), 0))
+      current = path[current]
+    end #while
+
+    current = to
+
+    while current != from
+      flow[(path[current], current)] += deltaFlow
+      flow[(current, path[current])] -= deltaFlow
+      current = path[current]
+    end #while
+
+  end #while
+
+  return flow
+end #EdmondsKarp
+
+@inline function randomWeight(cube::Hypercube, from::UInt16, to::UInt16)::UInt32
+  upperBound = max(hammingWeight(from), hammingWeight(to), cube.size, cube.size - hammingWeight(to))
+  return UInt32(rand(1:upperBound))
+end
+
+function randomHyperCube(size::Number)::Hypercube
+  cube = Hypercube(size)
+  for from::UInt16 in 0:2^size-1
+    for to::UInt16 in getNeighbours(cube, from)
+      setEdgeWeight!(cube, from, to, randomWeight(cube, from, to))
+    end #for
+  end #for
+  return cube
+end #randomHyperCube
 
 end #HypercubeGraph
---
+++ lista4/hypercube_test.jl
@@ -0,0 +1,82 @@
+include("hypercube.jl")
+using .HypercubeGraph
+using Test
+
+
+@testset "Get/Set edge" begin
+  cube = Hypercube(4)
+
+  @test getEdgeWeight(cube, UInt16(0), UInt16(1)) == UInt32(1)
+  @test getEdgeWeight(cube, UInt16(0), UInt16(2)) == UInt32(1)
+  setEdgeWeight!(cube, UInt16(0), UInt16(1), UInt32(10))
+  setEdgeWeight!(cube, UInt16(0), UInt16(2), UInt32(20))
+  @test getEdgeWeight(cube, UInt16(0), UInt16(1)) == UInt32(10)
+  @test getEdgeWeight(cube, UInt16(0), UInt16(2)) == UInt32(20)
+
+  setEdgeWeight!(cube, UInt16(11), UInt16(15), UInt32(3))
+  setEdgeWeight!(cube, UInt16(14), UInt16(15), UInt32(4))
+
+  @test getEdgeWeight(cube, UInt16(11), UInt16(15)) == UInt32(3)
+  @test getEdgeWeight(cube, UInt16(14), UInt16(15)) == UInt32(4)
+
+end #testset
+
+@testset "Get neighbours" begin
+  cube = Hypercube(4)
+
+  @test Set(getNeighbours(cube, UInt16(0))) == Set([UInt16(1), UInt16(2), UInt16(4), UInt16(8)])
+  @test Set(getNeighbours(cube, UInt16(1))) == Set([UInt16(3), UInt16(5), UInt16(9)])
+  @test Set(getNeighbours(cube, UInt16(2))) == Set([UInt16(3), UInt16(6), UInt16(10)])
+  @test Set(getNeighbours(cube, UInt16(3))) == Set([UInt16(7), UInt16(11)])
+
+  @test Set(getNeighbours(cube, UInt16(4))) == Set([UInt16(5), UInt16(6), UInt16(12)])
+  @test Set(getNeighbours(cube, UInt16(5))) == Set([UInt16(7), UInt16(13)])
+  @test Set(getNeighbours(cube, UInt16(6))) == Set([UInt16(7), UInt16(14)])
+  @test Set(getNeighbours(cube, UInt16(7))) == Set([UInt16(15)])
+
+  @test Set(getNeighbours(cube, UInt16(8))) == Set([UInt16(9), UInt16(10), UInt16(12)])
+  @test Set(getNeighbours(cube, UInt16(9))) == Set([UInt16(11), UInt16(13)])
+  @test Set(getNeighbours(cube, UInt16(10))) == Set([UInt16(11), UInt16(14)])
+  @test Set(getNeighbours(cube, UInt16(11))) == Set([UInt16(15)])
+
+  @test Set(getNeighbours(cube, UInt16(12))) == Set([UInt16(13), UInt16(14)])
+  @test Set(getNeighbours(cube, UInt16(13))) == Set([UInt16(15)])
+  @test Set(getNeighbours(cube, UInt16(14))) == Set([UInt16(15)])
+  @test Set(getNeighbours(cube, UInt16(15))) == Set([])
+
+end #testset
+
+@testset "Get reverse neighbours" begin
+  cube = Hypercube(4)
+
+  @test Set(getReverseNeighbours(cube, UInt16(0))) == Set([])
+  @test Set(getReverseNeighbours(cube, UInt16(1))) == Set([UInt16(0)])
+  @test Set(getReverseNeighbours(cube, UInt16(2))) == Set([UInt16(0)])
+  @test Set(getReverseNeighbours(cube, UInt16(3))) == Set([UInt16(1), UInt16(2)])
+
+  @test Set(getReverseNeighbours(cube, UInt16(4))) == Set([UInt16(0)])
+  @test Set(getReverseNeighbours(cube, UInt16(5))) == Set([UInt16(1), UInt16(4)])
+  @test Set(getReverseNeighbours(cube, UInt16(6))) == Set([UInt16(2), UInt16(4)])
+  @test Set(getReverseNeighbours(cube, UInt16(7))) == Set([UInt16(3), UInt16(5), UInt16(6)])
+
+  @test Set(getReverseNeighbours(cube, UInt16(8))) == Set([UInt16(0)])
+  @test Set(getReverseNeighbours(cube, UInt16(9))) == Set([UInt16(1), UInt16(8)])
+  @test Set(getReverseNeighbours(cube, UInt16(10))) == Set([UInt16(2), UInt16(8)])
+  @test Set(getReverseNeighbours(cube, UInt16(11))) == Set([UInt16(3), UInt16(9), UInt16(10)])
+
+  @test Set(getReverseNeighbours(cube, UInt16(12))) == Set([UInt16(4), UInt16(8)])
+  @test Set(getReverseNeighbours(cube, UInt16(13))) == Set([UInt16(5), UInt16(9), UInt16(12)])
+  @test Set(getReverseNeighbours(cube, UInt16(14))) == Set([UInt16(6), UInt16(10), UInt16(12)])
+  @test Set(getReverseNeighbours(cube, UInt16(15))) == Set([UInt16(7), UInt16(11), UInt16(13), UInt16(14)])
+
+end #testset
+
+@testset "Random hypercube" begin
+  cube = randomHyperCube(4)
+
+  for i in 1:15
+    for e in getReverseNeighbours(cube, UInt16(i))
+      @test getEdgeWeight(cube, UInt16(i), e) == UInt32(0)
+    end
+  end
+end

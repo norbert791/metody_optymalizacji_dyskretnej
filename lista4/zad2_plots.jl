df31f394b26b1b4a7eec1f290e0c287c447629ea
Norbert Jaśniewicz <norbert.jasniewicz@protonmail.com>
2023-06-15 02:00:30 +0200

    Bugix for zad2_plots for lista4

--- lista4/zad2_plots.jl
+++ lista4/zad2_plots.jl
@@ -7,7 +7,7 @@ function main()
     df = CSV.read("experiments/zad2_k$k.csv", DataFrame)
     #Plot avgFlow, avgAugmentingPaths, avgTime
 
-    t = Plots.scatter(df[!, :i], df[!, :avgMaxMatching], title="Avg max matching", xlabel="i", ylabel="max matching", label="Max matching")
+    t = Plots.scatter(df[!, :i], df[!, :avgMaxMatching], title="Avg max matching (k=$k)", xlabel="i", ylabel="max matching", label="Max matching")
     Plots.savefig(t, "plots/zad2_k$k.png")
   end #for
 
@@ -15,7 +15,7 @@ function main()
     df = CSV.read("experiments/zad2_i$i.csv", DataFrame)
     #Plot avgFlow, avgAugmentingPaths, avgTime
 
-    t = Plots.scatter(df[!, :k], df[!, :avgTime], title="Avg time", xlabel="k", ylabel="time", label="Time")
+    t = Plots.scatter(df[!, :k], df[!, :avgTime], title="Avg time (i=$i)", xlabel="k", ylabel="time", label="Time")
     Plots.savefig(t, "plots/zad2_i$i.png")
   end #for
 end #main
